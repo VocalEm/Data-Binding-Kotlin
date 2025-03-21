c2f084cbe0d07268b3895124ca97e7dcb6d8cf92
Jose Emiliano Frias Felix <Emilianofriasfelix@outlook.com>
2025-03-20 23:10:38 -0600

    se creoo el modelo

---
+++ app/src/main/java/com/equipo10/myapplicationbuildfeature/ColorModel.kt
@@ -0,0 +1,6 @@
+package com.equipo10.myapplicationbuildfeature
+
+data class ColorModel(
+    val colorName: String,
+    val colorimg: Int
+)
--- app/src/main/java/com/equipo10/myapplicationbuildfeature/MainActivity.kt
+++ app/src/main/java/com/equipo10/myapplicationbuildfeature/MainActivity.kt
@@ -2,12 +2,14 @@ package com.equipo10.myapplicationbuildfeature
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.equipo10.myapplicationbuildfeature.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  ColorModels : MutableList<ColorModel>
     private lateinit var binding: ActivityMainBinding // Definimos `binding` como una propiedad lateinit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +18,11 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main) // Inicializamos aquí
         binding.mainActivity = this // Vincula la actividad con el layout
         }
+
+    fun setupColorModels() {
+        var imageView: ImageView
+        var ColorNames = binding.root.context.resources.getStringArray(R.array.strings_colors)
+    }
 
     fun onSendClick() {
         val text = binding.editText.text.toString()
